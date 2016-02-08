437bdf8df6370f058bde4ecc99da7550190cd948
Chris McCready <wjb12175@uni.strath.ac.uk>
2016-02-08 13:13:34 +0000

    javascript sign up code working so far, need to make the php now

--- UserValidation.js
+++ UserValidation.js
@@ -14,6 +14,7 @@ function detailsValidation(){
            if(user_details[i] === ""){
                details_okay = false;
                break;
+               // will also need to check the lengths of the input so they are not too long               
            }
        }
        
@@ -24,6 +25,30 @@ function detailsValidation(){
        console.log(details_okay);
        
        // if details_okay call the php
+       if(details_okay){
+           this.signUp(user_details);
+       }
+    };
+    
+    // this is the function that calls the php for signing up
+    this.signUp = function(user_details){        
+        var table_entry = "username=" + user_details[0] + "&password=" + user_details[1];
+        var xmlhttp;
+        if(window.XMLHttpRequest){
+            // chrome, firefox, safari
+            xmlhttp = new XMLHttpRequest();
+        }
+        else{
+            // code for IE6, IE5
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        // remember to create the php page
+        xmlhttp.open("POST", "signUp.php", false);
+        xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xmlhttp.onreadystatechange = function () {
+            // in here we can use the response text to check if the sign up worked
+        };
+        xmlhttp.send(table_entry);
     };
     
 }
