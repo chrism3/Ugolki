83129c3a387d440c1f37c20a26bc814d5109bb69
Chris McCready <wjb12175@uni.strath.ac.uk>
2016-03-01 10:18:42 +0000

    made the basis for the medium AI, only choses random moves currently

--- mediumAI.js
+++ mediumAI.js
@@ -11,12 +11,61 @@ function mediumAI(){
     var board;
     var choosen_move;
     var piece_index;
+    var origional_x;
+    var origional_y;
     
     this.mediumAI = function(pieces, board_rep){
             board = board_rep;
-            var AI = new generalAI();            
-                         
+            var AI = new generalAI();
+            var all_moves = AI.evalAllMoves(AI.findAllMoves(pieces, board));
+            //console.log(all_moves);
+            var good_moves = all_moves[0];
+            var bad_moves = all_moves[1];
+            this.decideBestMove(good_moves, bad_moves, AI);                         
     }; 
+    
+    // this is how the better AI selects it's move
+    this.decideBestMove = function(good_moves, bad_moves, AI){
+        // need to make sure there is a valid target
+        this.updateTarget(AI);
+        
+        var random_index = parseInt(Math.random()*good_moves.length);
+        this.setChoosenMove(good_moves[random_index][0]);
+        var random_piece = good_moves[random_index][0].getPieceToMove();
+        this.setSelectedPieceIndex(random_piece.getPieceId());
+        this.setAISelectedPieceXCoord(random_piece.getXCoord());
+        this.setAISelectedPieceYCoord(random_piece.getYCoord());
+    };
+    
+    this.updateTarget = function(AI){
+//        console.log("target location contains: " + board_representation[this.getTargetX()][this.getTargetY()]);
+//        if(board_representation[this.getTargetX()][this.getTargetY()] !== 0){
+//            console.log(board_representation[this.getTargetX()][this.getTargetY()].getPieceColour());
+//        }
+        var target_free = false;
+        do{
+            // update "black" to be AI.getPieceColour() ?
+            if(board[AI.getTargetX()][AI.getTargetY()] !== 0 &&
+                    board[AI.getTargetX()][AI.getTargetY()].getPieceColour() === "black"){
+                console.log("inside the if");
+                var x = AI.getTargetX();
+                var y = AI.getTargetY();
+                
+                if(y < 4){
+                    AI.setTargetY(y + 1);
+                }
+                else{
+                    AI.setTargetX(x + 1);
+                    AI.setTargetY(0);
+                }
+                console.log("updating target");
+            }
+            else{
+                target_free = true;
+            }
+        }
+        while(!target_free);
+    };
     
     
     this.setChoosenMove = function(move){
@@ -31,6 +80,18 @@ function mediumAI(){
     };
     this.setSelectedPieceIndex = function(index){
         piece_index = index;
+    };
+        this.getAISelectedPieceXCoord = function(){
+        return origional_x;
+    };
+    this.setAISelectedPieceXCoord = function(x){
+        origional_x = x;
+    };
+    this.getAISelectedPieceYCoord = function(){
+        return origional_y;
+    };
+    this.setAISelectedPieceYCoord = function(y){
+        origional_y = y;
     };
 
 }
